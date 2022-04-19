5e827a12ad1e03f47d7877ae0a728f73656fc7d1
Hana Lipschutz <hanalif619@gmail.com>
2022-04-19 23:39:52 +0300

    app component takeuntil to unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, ReplaySubject, Subject, takeUntil } from 'rxjs';
 import { Animations } from './animations'
 import { CartService } from './services/cart.service';
 import { UIService } from './services/UI.service';
@@ -12,24 +12,31 @@ import { UIQuery } from './state/UI/UIQuery';
   animations: [Animations.mobileMenuAnimation, Animations.cartAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
  public showWithBackdrop!: boolean;
  public showMobileMenu!: boolean;
  isMobileMenuOpen$!: Observable<boolean>;
  isCartOpen$!: Observable<boolean>;
+ private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
  constructor( private uIQuery: UIQuery, private uIService:UIService, private cartService: CartService) { }
+
   ngOnInit(): void {
     this.isMobileMenuOpen$ = this.uIQuery.setIsMenuMobileOpen();
     this.isCartOpen$ = this.uIQuery.setIsCartOpen();
-    this.uIService.getItemsCategories().subscribe(); // todo add routing to app and move to resolver
-    this.uIService.updateWhenUrlChangesOccur().subscribe();
-    this.cartService.loadItemsOrderInfoFromStorage().subscribe();
+    this.uIService.getItemsCategories().pipe(takeUntil(this.destroyed$)).subscribe(); // todo add routing to app and move to resolver
+    this.uIService.updateWhenUrlChangesOccur().pipe(takeUntil(this.destroyed$)).subscribe();
+    this.cartService.loadItemsOrderInfoFromStorage().pipe(takeUntil(this.destroyed$)).subscribe();
   }
 
 
   onBackdropClicked(val:boolean){
     this.uIService.setUiStoreAfterBackdropClicked(val);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.unsubscribe();
   }
 
 }
