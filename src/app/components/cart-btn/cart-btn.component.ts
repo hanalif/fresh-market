24edb26aa44e75a83b8b9b0368d2f701c4f34f4e
Hana Lipschutz <hanalif619@gmail.com>
2022-04-19 07:49:25 +0300

    fix number of items in cart

--- src/app/components/cart-btn/cart-btn.component.ts
+++ src/app/components/cart-btn/cart-btn.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ItemQuery } from 'src/app/modules/items/state/itemQuery';
 
 @Component({
   selector: 'app-cart-btn',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-btn.component.scss']
 })
 export class CartBtnComponent implements OnInit {
+  cartItemsLength$! :Observable<number>;
 
-  constructor() { }
+  constructor(private itemQuery:ItemQuery) { }
 
   ngOnInit(): void {
+    this.cartItemsLength$ = this.itemQuery.getItemsToShowInCart().pipe(
+      map(itemsToShowInCart=> itemsToShowInCart.length)
+    )
   }
 
 }
