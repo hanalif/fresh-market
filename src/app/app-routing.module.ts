b52fde1655d453b4962c1b28f087d3a2820cc026
Hana Lipschutz <hanalif619@gmail.com>
2022-06-27 18:36:06 +0300

    cart design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CartComponent } from './components/header/cart/cart.component';
 
 const routes: Routes = [
 
